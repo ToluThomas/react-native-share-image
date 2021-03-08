bf15167c45635be769218c68d03808179f869da3
Tolu Thomas <tolulope.thomas@interswitchgroup.com>
2021-03-08 22:04:23 +0100

    Removed unused

--- android/src/main/java/com/toluthomas/rnshareimage/RNShareImagePackage.java
+++ android/src/main/java/com/toluthomas/rnshareimage/RNShareImagePackage.java
@@ -1,9 +1,7 @@
 
 package com.toluthomas.rnshareimage;
 
-import java.lang.annotation.Native;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,7 +9,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
 
 import javax.annotation.Nonnull;
 
