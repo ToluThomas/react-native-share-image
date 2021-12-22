0c3ee8b38e073b06067b3076d1775741549672ce
Tolu Thomas <tolu.thomas@protonmail.com>
2021-12-22 18:32:32 +0100

    Updated to match new parameters in index.js

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,11 @@
 declare module 'react-native-share-image' {
     /**
      * Share screenshot of current app screen or specify a view to get screenshot of
-     * @param {string} message - Message to be shown during share
-     * @param {string} filename - name of temporary screenshot file
      * @param {string} id - ID for a View which is set using the nativeID prop for Views
      * @link See https://reactnative.dev/docs/view#nativeid for how to set nativeID
+     * @param {string} message - Message to be shown during share
+     * @param {string} filename - name of temporary screenshot file
+     * @param {string} shareTitle - title of share modal
      */
     export function shareScreenshot(): void
 
@@ -12,6 +13,7 @@ declare module 'react-native-share-image' {
      * Share image using uri of an image
      * @param {string} imageUri - Specify a content uri for image to be shared
      * @param {string} message - Message to be shown during share
+     * @param {string} shareTitle - title of share modal
      */
     export function shareImageFromUri(): void
 }
